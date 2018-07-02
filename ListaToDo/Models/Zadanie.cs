5d9ad7327fcf36e189d324d4a304867455c5efa1
marek2222 <marekmusial2@o2.pl>
2018-07-02 22:08:46 +0200

    ListaToDo_Mvc:  1.04: Klasa Zadanie.cs w Models

---
+++ ListaToDo/Models/Zadanie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ListaToDo.Models
+{
+  public class Zadanie
+  {
+    public Zadanie()    {}
+
+    [Display(Name = "Id:")]
+    public int Id { get; set; }
+
+    [Required]
+    [Display(Name = "Nazwa zadania"), MaxLength(50)]
+    public string Nazwa { get; set; }
+
+    [Display(Name = "Opis zadania"), MaxLength(2000)]
+    public string Opis { get; set; }
+
+    [Display(Name = "Termin wykonania"), DataType(DataType.DateTime)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:MM tt}", ApplyFormatInEditMode = true)]
+    public System.DateTime Termin  { get; set; }
+
+    // wykonane- 1, niewykonane - 0)
+    public bool Status { get; set; }
+
+    //- Nazwa zadania(max 50zn, pole obowiązkowe)
+    //- opis zadania(max. 2000 zn, pole nieobwiązkowe)
+    //- Termin wykonania(data i czas wykonania, pole nieobowiązkowe)
+    //- Status(wykonane/niewykonane)
+  }
+}
